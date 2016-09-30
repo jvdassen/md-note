0c4174981ed6f8a8df895e03aa0dd8ff7e2da1d8
Jan von der Assen <jan.vda@gmx.ch>
2016-09-30 10:56:37 +0200

    initial commit

---
+++ md-note/client/main.js
@@ -0,0 +1,56 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import './main.html';
+Markdown = new Mongo.Collection('markdown');
+
+var editmode = false;
+
+jQuery( document ).ready(function() {
+
+  console.log(Markdown.findOne());
+  setTimeout(function(){console.log(Markdown.findOne());}, 4000);
+
+  jQuery("textarea").keyup(function() {
+    var converter = new showdown.Converter(),
+        text      = jQuery('textarea').val(),
+        html      = converter.makeHtml(text);
+
+    jQuery('#md-display').html(html);
+    console.log(Markdown.insert({md: text, type: "save"}));
+  });
+  jQuery('#mode-btn').click(function(){
+    if (editmode) {
+      jQuery('#display-wrapper').css('width', '100%');
+      jQuery('#input-wrapper').hide();
+      editmode = false;
+      jQuery('#mode-btn').text('Show');
+    }else {
+      jQuery('#display-wrapper').css('width', '50%');
+      jQuery('#input-wrapper').show();
+      editmode = true;
+      jQuery('#mode-btn').text('Hide');
+    }
+  });
+
+});
+
+
+
+//
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
---
+++ md-note/server/main.js
@@ -0,0 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+Markdown = new Mongo.Collection('markdown');
+Meteor.startup(() => {
+  // code to run on server at startup
+
+});
